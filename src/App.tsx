b6382bf68ad6d0d9f438696430c1706c18c5dd5f
normalperson543 <148504965+normalperson543@users.noreply.github.com>
2026-03-15 22:25:05 -0700

    refining the header and also adding keycap animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import "./App.css";
 import orangePaper from "./assets/orange-paper.jpg";
 import paper from "./assets/paper.jpg";
-
+import r65 from "./assets/keebs/r65.png"
+import f75 from "./assets/keebs/f75.png"
+import hcKeycap from "./assets/keycaps/hc.png"
+import { motion } from "motion/react";
 function App() {
   return (
     <div className="w-full flex flex-col gap-2">
@@ -16,8 +19,11 @@ function App() {
             style={{ backgroundImage: `url(${paper})` }}
           >
             keyboard emporium!
-            <div className="p-4 rounded-md">
-              
+            <div className="p-4 -right-8 -bottom-8 rotate-5 border-dashed w-fit absolute hover:rotate-10 hover:scale-120 duration-200">
+              <img src={r65} width={120} height={120} />
+            </div>
+            <div className="p-4 -left-8 -top-8 rotate-5 border-dashed w-fit absolute hover:-rotate-10 hover:scale-120 duration-200">
+              <img src={f75} width={120} height={120} />
             </div>
           </div>
           <div className="text-4xl flex flex-col gap-8">
@@ -33,17 +39,23 @@ function App() {
           </div>
           <input
             type="email"
-            className="border border-orange-900 bg-orange-100 p-4 text-4xl cursor-pointer hover:scale-102 w-full"
+            className="border border-orange-900 bg-orange-100 p-4 text-4xl cursor-pointer hover:scale-102 w-full duration-200"
             placeholder="enter your email (teens 13-18)"
           />
           <div className="flex gap-4 w-full">
-            <div className="border border-orange-900 bg-orange-200 p-4 text-4xl cursor-pointer hover:scale-102 hover:rotate-1 flex-1">
+            <div className="border border-orange-900 bg-orange-200 p-4 text-4xl cursor-pointer hover:scale-102 hover:rotate-1 flex-1 duration-200">
               preview platform!
             </div>
-            <div className="border border-orange-900 bg-orange-100 p-4 text-4xl cursor-pointer hover:scale-102 hover:rotate-1 flex-1">
+            <div className="border border-orange-900 bg-orange-100 p-4 text-4xl cursor-pointer hover:scale-102 hover:rotate-1 flex-1 duration-200">
               how it works
             </div>
           </div>
+          <motion.img src={hcKeycap} className="absolute top-12" width={150} height={150} initial={{left: -24, opacity: 0, rotate: 0}} animate={{left: 24, opacity: 1, rotate: 12}} transition={{type: "spring"}}/>
+          <motion.img src={hcKeycap} className="absolute top-60" width={150} height={150} initial={{left: -24, opacity: 0, rotate: 0}} animate={{left: 24, opacity: 1, rotate: -12}} transition={{type: "spring", delay: 0.1}}/>
+          <motion.img src={hcKeycap} className="absolute top-120" width={150} height={150} initial={{left: -24, opacity: 0, rotate: 0}} animate={{left: 24, opacity: 1, rotate: 12}} transition={{type: "spring", delay: 0.2}}/>
+          <motion.img src={hcKeycap} className="absolute top-12" width={150} height={150} initial={{right: -24, opacity: 0, rotate: 0}} animate={{right: 24, opacity: 1, rotate: -12}} transition={{type: "spring", delay: 0.25}}/>
+          <motion.img src={hcKeycap} className="absolute top-60" width={150} height={150} initial={{right: -24, opacity: 0, rotate: 0}} animate={{right: 24, opacity: 1, rotate: 12}} transition={{type: "spring", delay: 0.3}}/>
+          <motion.img src={hcKeycap} className="absolute top-120" width={150} height={150} initial={{right: -24, opacity: 0, rotate: 0}} animate={{right: 24, opacity: 1, rotate: -12}} transition={{type: "spring", delay: 0.32}}/>
         </div>
       </div>
     </div>
